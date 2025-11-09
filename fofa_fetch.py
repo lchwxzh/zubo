72ce21a63e353087366830c1ac4d3c060aed320a
lchwxzh <lchwxzh@163.com>
2025-11-09 08:21:56 +0800

    Update fofa_fetch.py

--- fofa_fetch.py
+++ fofa_fetch.py
@@ -353,7 +353,7 @@ def third_stage():
                 valid_lines.append(f"{c},{u}${province_operator}")
 
     beijing_now = datetime.now(timezone(timedelta(hours=8))).strftime("%Y-%m-%d %H:%M:%S")
-    disclaimer_url ="http://live.njgdmm.com/changzhi/cztv1.m3u8,http://live.njgdmm.com/changzhi/cztv2.m3u8,http://live.njgdmm.com/changzhi/czetv.m3u8"
+    disclaimer_url ="https://13509.kstore.space/bdpd.txt"
                     
     with open(IPTV_FILE, "w", encoding="utf-8") as f:
         f.write(f"更新时间: {beijing_now}（北京时间）\n\n")
@@ -395,6 +395,7 @@ if __name__ == "__main__":
 
 
     
+
 
 
 
